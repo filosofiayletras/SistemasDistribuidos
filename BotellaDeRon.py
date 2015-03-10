184e107cf4ca6685e0ee81241b4f8a5917e76124
Manuel F <shakebeforeuse@gmail.com>
2015-03-10 18:06:26 +0100

    Implementada canción "99 Bottles of Beer"

---
+++ BotellaDeRon.py
@@ -0,0 +1,2 @@
+for i in range(99, 0, -1):
+	print (str(i) + " bottles of beer on the wall, " + str(i) + " bottles of beer.\nTake one down, pass it around, " + str(i-1) + " bottles of beer on the wall.")
