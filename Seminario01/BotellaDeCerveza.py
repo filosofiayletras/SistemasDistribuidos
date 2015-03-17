6d39b080450ca3fab10f23e259c82de6b4844e3d
Manuel F <shakebeforeuse@gmail.com>
2015-03-17 15:38:34 +0100

    Modificada la carpeta del primer seminario y Readme.md

--- Seminario01/BotellaDeCerveza.py
+++ Seminario01/BotellaDeCerveza.py
@@ -1,2 +1,7 @@
+#Grupo 21
+#Jose Manuel Vidal
+#Manuel Francisco
+#Seminario 01 de Sistemas Distribudios
+
 for i in range(99, 0, -1):
 	print(i, " bottles of beer on the wall, ", i, " bottles of beer.\nTake one down, pass it around, ", i-1, " bottles of beer on the wall.")
